1404487796ea66288ff4f3b588f0bfa94f754df1
lmmx <limingxue@sto.cn>
2019-10-11 14:13:06 +0800

    dev first commit

--- app-gateway/src/main/java/com/sto/AppGatewayApplication.java
+++ app-gateway/src/main/java/com/sto/AppGatewayApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 	网关
+ * @author limingxue
+ *
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AppGatewayApplication {
